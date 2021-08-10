b55a3af219901028cec550a24ba025028c850694
Steven24K <s.koerts2@gmail.com>
2021-08-10 20:09:54 +0200

    Make React Hooks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, NavLink, Route, Switch, } from 'react-router-dom'
 import '../static/css/site.css'
 
 const data: string[] = [
@@ -14,48 +14,44 @@ interface AppState {
     search: string
 }
 
+const zeroAppState = (): AppState => ({ search: '' })
 interface AppProps {
 
 }
 
-export class App extends React.Component<AppProps, AppState> {
-    constructor(props: AppProps) {
-        super(props)
-        this.state = { search: '' }
-    }
+export const App = (props: AppProps) => {
+    const [appState, setState] = React.useState<AppState>(zeroAppState())
 
-    render() {
-        return <div className="App">
-            <h1>Hello React Typescript</h1>
-            <p>Start editing the <code>App.tsx</code> file to get started</p>
+    return <div className="App">
+        <h1>Hello React Typescript</h1>
+        <p>Start editing the <code>App.tsx</code> file to get started</p>
 
-            <input
-                placeholder="Search to filter"
-                value={this.state.search}
-                onChange={e => this.setState(s => ({ ...s, search: e.target.value }))}
-            />
+        <input
+            placeholder="Search to filter"
+            value={appState.search}
+            onChange={e => setState(s => ({ ...s, search: e.target.value }))}
+        />
 
-            <ul>
-                {data.filter(d => this.state.search.includes(d) || d.includes(this.state.search) || this.state.search == '').map(d => <li key={d}>{d}</li>)}
-            </ul>
+        <ul>
+            {data.filter(d => appState.search.includes(d) || d.includes(appState.search) || appState.search == '').map(d => <li key={d}>{d}</li>)}
+        </ul>
 
-            <h3>Hot module reloading is on!</h3>
+        <h3>Hot module reloading is on!</h3>
 
-            {/**Small routing example */}
-            <BrowserRouter>
-                <nav>
-                    <ul>
-                        <li><NavLink to='/'>Home</NavLink></li>
-                        <li><NavLink to='/about'>About</NavLink></li>
-                        <li><NavLink to='/contact'>Contact</NavLink></li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path='/' exact render={() => <h1>Home</h1>}/>
-                    <Route path='/about' render={() => <h1>About</h1>}/>
-                    <Route path='/contact' render={() => <h1>Contact</h1>}/>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    }
+        {/**Small routing example */}
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/about'>About</NavLink></li>
+                    <li><NavLink to='/contact'>Contact</NavLink></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path='/' exact render={() => <h1>Home</h1>} />
+                <Route path='/about' render={() => <h1>About</h1>} />
+                <Route path='/contact' render={() => <h1>Contact</h1>} />
+            </Switch>
+        </BrowserRouter>
+    </div>
 }
